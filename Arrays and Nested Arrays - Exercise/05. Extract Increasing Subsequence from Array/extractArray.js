59429617783e814d3f789d2d143a09539dcfbe0b
Mariela <marielamiteva97@abv.bg>
2022-10-12 06:45:54 -0700

    05.Extract Increasing Subsequence from Array

---
+++ Arrays and Nested Arrays - Exercise/05. Extract Increasing Subsequence from Array/extractArray.js
@@ -0,0 +1,13 @@
+function solve(arr) {
+let result = [];
+let biggest = arr[0];
+for(let index = 0; index < arr.length; index++) {
+    let currentNum = arr[index];
+    if(currentNum >= biggest) {
+        result.push(currentNum)
+        biggest = currentNum;
+    }
+}
+return result;
+}
+ console.log(solve([1, 3, 8, 4, 10, 12, 3, 2, 24]))
