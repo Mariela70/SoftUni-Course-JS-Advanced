f1b57923426476c95c3df06179097d5bd0d6055e
Mariela <marielamiteva97@abv.bg>
2022-10-13 09:18:46 -0700

    01.City Record

---
+++ Objects and Composition - Lab/01. City Record/cityRecord.js
@@ -0,0 +1,11 @@
+function city(name, population, treasury) {
+const result = {
+    name: name,
+    population: population,
+    treasury: treasury
+}
+return result;
+}
+console.log(city('Tortuga',
+7000,
+15000))
