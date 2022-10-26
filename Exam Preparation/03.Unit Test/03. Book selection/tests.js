9d7f2e6416adba17b5a55dadffcb540469a9dcd7
Mariela <marielamiteva97@abv.bg>
2022-10-26 13:54:39 -0700

    03.Book selection

---
+++ Exam Preparation/03.Unit Test/03. Book selection/tests.js
@@ -0,0 +1,69 @@
+const { expect } = require('chai');
+const { bookSelection } = require('./solution');
+
+describe("Book Selection", () => {
+  describe("isGenreSuitable", () => {
+    it('happy path', () => {
+      expect(bookSelection.isGenreSuitable('Thriller', 20)).to.equal('Those books are suitable');
+      expect(bookSelection.isGenreSuitable('Horror', 20)).to.equal('Those books are suitable');
+      expect(bookSelection.isGenreSuitable('a', 20)).to.equal('Those books are suitable');
+      expect(bookSelection.isGenreSuitable('a', 10)).to.equal('Those books are suitable');
+    });
+    it('inappropriate genre for the age', () => {
+      expect(bookSelection.isGenreSuitable('Thriller', 10)).to.equal('Books with Thriller genre are not suitable for kids at 10 age');
+      expect(bookSelection.isGenreSuitable('Horror', 10)).to.equal('Books with Thriller genre are not suitable for kids at 10 age')
+    });
+    it('inappropriate genre for the age second case', () => {
+      expect(bookSelection.isGenreSuitable('Thriller', 12)).to.equal('Books with Thriller genre are not suitable for kids at 12 age');
+      expect(bookSelection.isGenreSuitable('Horror', 12)).to.equal('Books with Thriller genre are not suitable for kids at 12 age')
+    });
+  });
+});
+describe('isItAffordable', () => {
+  it('happy path', () => {
+    expect(bookSelection.isItAffordable(1, 2)).to.equal('Book bought. You have 1$ left');
+  });
+  it('happy path second case', () => {
+    expect(bookSelection.isItAffordable(1, 1)).to.equal('Book bought. You have 0$ left');
+  });
+  it('not enough money', () => {
+    expect(bookSelection.isItAffordable(2, 1)).to.equal('You don\'t have enough money');
+  });
+  it('Invalid input', () => {
+    expect(() => bookSelection.isItAffordable('1', 1)).to.throw();
+    expect(() => bookSelection.isItAffordable(1, '1')).to.throw();
+    expect(() => bookSelection.isItAffordable('1', '1')).to.throw();
+  })
+
+});
+describe('suitableTitles', () => {
+  it('happy path', () => {
+    expect(bookSelection.suitableTitles([{
+      title: 'aa',
+      genre: 'a'
+    }], 'a')).to.deep.equal(['aa'])
+  });
+  it('happy path second case', () => {
+    expect(bookSelection.suitableTitles([{
+      title: 'aa',
+      genre: 'a'
+    },
+    {
+      title: 'ab',
+      genre: 'a'
+    },
+    {
+      title: 'bb',
+      genre: 'b'
+    }], 'a')).to.deep.equal(['aa', 'ab'])
+  });
+  it('no match', () => {
+    expect(bookSelection.suitableTitles([{
+      title: 'bb',
+      genre: 'b'
+    }], 'a')).to.deep.equal([])
+  });
+  it('Invalid input', () => {
+    expect(() => bookSelection.suitableTitles('aabb', 'a')).to.throw();
+  });
+});
